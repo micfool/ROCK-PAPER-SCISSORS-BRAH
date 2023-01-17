29292ebfc10c3fa3350d46e6924cd629f4a5689b
micfool <80862054+micfool@users.noreply.github.com>
2023-01-17 14:31:39 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const playerText = document.querySelector('#playerText');
+const computerText = document.querySelector('#computerText');
+const resultText = document.querySelector('#resultText');
+const choiceBtns = document.querySelectorAll(".choiceButton");
+let player;
+let computer; 
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));S
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random()*3)+1
+    switch (randNum){
+        case 1:
+            computer = "ROCK"
+            break;
+        case 2:
+            computer = "PAPER"
+            break;
+         case 3:
+            computer = "SCISSORS"
+            break;
+    }
+}
+
+function checkWinner(){
+    if(player == computer){
+        return "draw";
+    } else if (computer == "ROCK"){
+        return (player == "PAPER") ? "YOU WIN!" : "YOU LOSE"
+    }else if (computer == "PAPER"){
+        return (player == "SCISSORS") ? "YOU WIN!" : "YOU LOSE"
+    }else if (computer == "SCISSORS"){
+        return (player == "ROCK") ? "YOU WIN!" : "YOU LOSE"
+    }
+}
